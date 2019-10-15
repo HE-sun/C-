90cdde88024f65dc424afdcd7e0480f27f3d2afe
HE-sun <46038962+HE-sun@users.noreply.github.com>
2019-10-15 16:39:12 +0800

    Add files via upload

---
+++ practice01/practice01/practice01/SimpleFile.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "SimpleFile.h"
+
+
+SimpleFile::SimpleFile()
+{
+}
+
+
+//SimpleFile::~SimpleFile()
+//{
+//}
+
+BOOLEAN SimpleFile::ReadFile(WCHAR * pathName)
+{
+	wifstream inFile(pathName, ios::in);
+	if (!inFile)
+	{
+		return FALSE;
+	}
+	inFile.imbue(locale(locale(), "", LC_CTYPE));
+	wstring buffer;
+	while (inFile.good())
+	{
+		std::getline(inFile, buffer);
+		FileContent.push_back(buffer);
+	}
+	inFile.close();
+
+	return TRUE;
+}
+
+BOOLEAN SimpleFile::WriteFile(WCHAR * pathName, WCHAR * fileLines)
+{
+if (fileLines != NULL)
+	{
+		wstringstream mySS;
+		mySS << fileLines;
+		wofstream outFile(pathName, ios::out | ios::trunc);
+		if (!outFile)
+		{
+			return FALSE;
+		}
+		outFile.imbue(locale(locale(), "", LC_CTYPE));
+		wstring myString = mySS.str();
+		outFile.write(myString.c_str(), wcslen(fileLines));
+		outFile.close();
+	}
+	return TRUE;
+}
+
+vecLinesContainer SimpleFile::getFileLines(WCHAR* pathName)
+{
+	ReadFile(pathName);
+	return FileContent;
+}
